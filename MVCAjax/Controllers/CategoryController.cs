eb30ccff6afff1626b63b8d87b8c66c83a1cf264
UĞUR BAĞCI <ugur_bagci_@hotmail.com>
2019-06-21 15:30:00 +0300

    Add project files.

---
+++ MVCAjax/Controllers/CategoryController.cs
@@ -0,0 +1,56 @@
+﻿using MVCAjax.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCAjax.Controllers
+{
+    public class CategoryController : Controller
+    {
+        NorthwindEntities _service;
+
+        public CategoryController()
+        {
+            _service = new NorthwindEntities();
+        }
+
+        public ActionResult Create()
+        {
+            Category category = new Category();
+            return View(category);
+        }
+        [HttpPost]
+        public JsonResult Create(Category data)
+        {
+            Thread.Sleep(3000);
+            JsonResultVM jr = new JsonResultVM();
+            if (data.CategoryName == null)
+            {
+                jr.IsSuccess = false;
+                jr.UserMessage = "Kategori Adı Boş Geçilemez";
+            }
+            else
+            {
+                try
+                {
+                    _service.Categories.Add(data);
+                    _service.SaveChanges();
+
+                    jr.IsSuccess = true;
+                    jr.UserMessage = "Kategori başarıyla eklendi";
+                }
+                catch (Exception)
+                {
+                    jr.IsSuccess = false;
+                    jr.UserMessage = "Kayıt İşlemi Hatası";
+                    throw;
+                }
+            }
+            return Json(jr, JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
---
+++ MVCAjax/Models/JsonResultVM.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCAjax.Models
+{
+    public class JsonResultVM
+    {
+        public bool IsSuccess { get; set; }
+        public string UserMessage { get; set; }
+    }
+}
